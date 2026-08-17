b0752d3e52cff64c30faf111e165434a88add7cb
Soniya Gupta <soniyagupta@Soniyas-MacBook-Air.local>
2026-08-18 02:13:20 +0530

    updated import of WhatsAppChannel

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import AppSection from "./components/AppSection";
 import ConnectSection from "./components/ConnectSection";
 import FeatureBlock from "./components/FeatureBlock";
 import Hero from "./components/Hero";
-import WhatsAppChannel from "./components/WhatsappChannel";
+import WhatsAppChannel from "./components/WhatsAppChannel";
 
 function App() {
   return (

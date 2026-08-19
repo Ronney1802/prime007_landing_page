acac5b4ab9860f7874151d03fdbf17ac4a52b39f
Soniya Gupta <soniyagupta@Soniyas-MacBook-Air.local>
2026-08-19 22:28:55 +0530

    updated hero image usage, floating button and removed footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,11 @@ import AppSection from "./components/AppSection";
 import ConnectSection from "./components/ConnectSection";
 import FAQSection from "./components/FAQSection";
 import FeatureBlock from "./components/FeatureBlock";
-import Footer from "./components/Footer";
 import GameRates from "./components/GameRates";
 import Hero from "./components/Hero";
 import WhatsAppChannel from "./components/WhatsAppChannel";
-import prime007Logo from "./assets/images/prime007-logo(1).png";
-import { BRAND_NAME } from "./constants";
 import HowToPlay from "./components/HowToPlay";
+import FloatDownloadButton from "./components/FloatDownloadButton";
 
 function App() {
   return (
@@ -22,15 +20,8 @@ function App() {
       <HowToPlay />
       <AppSection />
       <FAQSection />
-      {/* <FloatDownloadButton /> */}
-      <div className="logo_container">
-        <img
-          src={prime007Logo}
-          className="prime007-logo"
-          alt={`${BRAND_NAME} app logo`}
-        />
-      </div>
-      <Footer />
+      <FloatDownloadButton />
+      {/* <Footer /> */}
     </div>
   );
 }
--- src/components/FloatDownloadButton.jsx
+++ src/components/FloatDownloadButton.jsx
@@ -1,5 +1,6 @@
 import { APK_DOWNLOAD_URL, BRAND_NAME } from "../constants";
 import "../styles/FloatDownloadButton.css";
+// import prime007Logo from "../assets/images/prime007-logo.png";
 
 const FloatDownloadButton = () => {
   return (
@@ -10,6 +11,13 @@ const FloatDownloadButton = () => {
             <i className="fas fa-download"></i>
           </div>
           <span className="float-download-text">{BRAND_NAME}</span>
+          {/* <div className="logo_container">
+            <img
+              src={prime007Logo}
+              className="prime007-logo"
+              alt={`${BRAND_NAME} app logo`}
+            />
+          </div> */}
         </a>
       </div>
     </section>

907c05a5254e8a836c48ab66afb3682c6e539726
Soniya Gupta <soniyagupta@Soniyas-MacBook-Air.local>
2026-08-20 15:01:28 +0530

    removed unnecessary variables and server plugging from vite config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,4 @@ import babel from "@rolldown/plugin-babel";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
-  server: {
-    allowedHosts: ["transport-unjustly-delicious.ngrok-free.dev"],
-  },
 });
